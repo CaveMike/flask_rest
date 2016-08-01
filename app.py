5d1afd9834d7b61f7ec9ab4d70e2241f04f3b18b
Mike Corrigan <corrigan@gmail.com>
2016-08-01 00:00:55 -0500

    Move database preperation to manage.py
    Move the database preperation logic out of the app
    and into manage.py.
    
    Signed-off-by: Mike Corrigan <corrigan@gmail.com>

--- app.py
+++ app.py
@@ -405,67 +405,6 @@ def prepare_routes(base_url='/api/v1.0/'):
 
     View.add(app, base_url=[base_url + 'users/<string:parent>/messages', base_url + 'messages'], endpoint='messages', model_cls=Message, schema_cls=MessageSchema, parent_cls=User)
 
-def prepare_database():
-    db = SqliteDatabase('peewee.db')
-    db.connect()
-    db.create_tables([Group, User, UserToGroup, Device, Publication, Subscription, Message], safe=True)
-
-    # Delete
-    Group.delete().execute()
-    User.delete().execute()
-    UserToGroup.delete().execute()
-    Device.delete().execute()
-    Publication.delete().execute()
-    Subscription.delete().execute()
-    Message.delete().execute()
-
-    # Groups
-    admin = Group.create(name='admin')
-    user = Group.create(name='user')
-    guest = Group.create(name='guest')
-
-    # Users
-    admin = User.create_user(name='Administrator', description='Administrator', email='admin@localhost.com', username='admin', password='1234')
-    Device.create(user=admin, name='d2', resource='work', type='computer', dev_id='a')
-
-    chloe = User.create_user(name='Chloe', username='chloe', password='1234')
-    d = chloe.create_device(name='d2', resource='work', type='computer', dev_id='a')
-    chloe.create_device(name='d0', resource='home', type='phone', dev_id='b')
-    chloe.create_device(name='d3', resource='home', type='laptop', dev_id='c')
-    chloe.create_device(name='d1', resource='work', type='phone', dev_id='d')
-    UserToGroup.create(user=chloe, group=guest)
-
-    sunshine = User.create_user(name='Sunshine', username='sunshine', password='1234')
-    sunshine.create_device(name='d5', resource='work', type='phone', dev_id='e')
-    UserToGroup.create(user=sunshine, group=user)
-    UserToGroup.create(user=sunshine, group=guest)
-    p = Publication.create(user=sunshine, topic='Life and Times of Sunshine', description='', group=guest)
-    Message.create(user=sunshine, to_publication=p, subject='First post!')
-    Message.create(user=sunshine, to_publication=p, subject='Eating breakfast')
-    Message.create(user=sunshine, to_publication=p, subject='Time for a nap')
-
-    guinness = User.create_user(name='Guinness', username='guinness', password='1234')
-    guinness.create_device(name='d7', resource='work', type='phone', dev_id='g')
-    UserToGroup.create(user=guinness, group=guest)
-
-    felix = User.create_user(name='Felix', username='felix', password='1234')
-    felix.create_device(name='d6', resource='work', type='phone', dev_id='f')
-    UserToGroup.create(user=felix, group=guest)
-    Subscription.create(user=felix, publication=p)
-    Message.create(user=felix, to_publication=p, subject='boring...')
-    Message.create(user=felix, to_user=sunshine, subject='hi sunshine')
-    Message.create(user=felix, to_device=d, subject='hi chloe')
-    Message.create(user=felix, to_user=chloe, subject='hi chloe again')
-
-    ducky = User.create_user(name='Ducky', username='ducky', password='1234')
-    ducky.create_device(name='d8', resource='work', type='phone', dev_id='h')
-    UserToGroup.create(user=ducky, group=admin)
-    UserToGroup.create(user=ducky, group=user)
-    UserToGroup.create(user=ducky, group=guest)
-
-    db.close()
-
 if __name__ == '__main__':
-    prepare_database()
     prepare_routes()
     app.run(debug=True)
