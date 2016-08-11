3916c471cccdc34a9a455ec9637cffd68673ec11
Mike Corrigan <corrigan@gmail.com>
2016-08-11 14:01:26 -0500

    Add auth filters
    Signed-off-by: Mike Corrigan <corrigan@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #!venv/bin/python
 import logging
 
+from functools import wraps
+
 import werkzeug.routing
 
 from flask import abort
 from flask import redirect
 from flask import Flask
+from flask import g
 from flask import jsonify
 from flask import make_response
 
@@ -41,12 +44,75 @@ database = FlaskDB(app, 'sqlite:///peewee.db')
 
 auth = HTTPBasicAuth()
 
+class AuthExt:
+    @classmethod
+    def save(cls, user, **kwargs):
+        g.current_user = user if user else None
+        admin_group = Group.select().where(Group.name == 'admin').get()
+        g.is_admin = admin_group.is_member(g.current_user)
+
+    @classmethod
+    def is_admin(cls, *args, **kwargs):
+        logging.debug('args={}, kwargs={}, g={}'.format(args, kwargs, g))
+        try:
+            return g.is_admin
+        except:
+            logging.exception('is_admin missing')
+
+        return False
+
+    @classmethod
+    def is_parent_user(cls, *args, **kwargs):
+        logging.debug('args={}, kwargs={}, g={}'.format(args, kwargs, g))
+        try:
+            if int(kwargs['parent']) == g.current_user.id:
+                return True
+        except KeyError:
+            logging.exception('parent not specified')
+        except TypeError:
+            logging.exception('invalid parent type')
+        except:
+            logging.exception('parent not user')
+
+        return False
+
+    @staticmethod
+    def admin_required(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            logging.debug('args={}, kwargs={}'.format(args, kwargs))
+
+            if AuthExt.is_admin(*args, **kwargs):
+                return f(*args, **kwargs)
+
+            return unauthorized()
+        return decorated
+
+    @staticmethod
+    def admin_or_parent(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            logging.debug('args={}, kwargs={}'.format(id, args, kwargs))
+
+            if AuthExt.is_admin(*args, **kwargs):
+                return f(*args, **kwargs)
+
+            if AuthExt.is_parent_user(*args, **kwargs):
+                return f(*args, **kwargs)
+
+            return unauthorized()
+        return decorated
+
 @auth.verify_password
 def verify_password(username, alleged_password):
     try:
         user = User.select().where(User.username == username).get()
         verification = (crypt(alleged_password, user.password) == user.password)
         logging.debug('verify_password: username={}, encrypted_password={}, alleged_password={}, verification={}'.format(username, user.password, alleged_password, verification))
+
+        if verification:
+            AuthExt.save(user=user)
+
         return verification
     except Exception as e:
         logging.exception('verify_password')
